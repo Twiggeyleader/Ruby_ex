f1bfd1971d5b090726f1982cb259b85b29ece05e
Mike Pacheco <mikelnpacheco@gmail.com>
2015-04-08 07:20:40 -0700

    first commit

---
+++ smoothie.rb
@@ -0,0 +1,11 @@
+ class Blender
+    def initialize
+    end
+
+    def blend(array)
+      array.join.split('').shuffle.join.gsub('')
+    end
+ end
+
+ # fixings =  ["bananas", "strawberry", "apple", "orange", "grape"]
+    
